3aa06ea51639175105f7e57c6d5f9e973109025e
ted555 <thanong984@gmail.com>
2017-04-19 10:05:35 +0700

    add new screen fragment

--- app/src/main/java/com/imaginecup/bug5/vista/activity/MainActivity.java
+++ app/src/main/java/com/imaginecup/bug5/vista/activity/MainActivity.java
@@ -1,16 +1,24 @@
 package com.imaginecup.bug5.vista.activity;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
 import com.github.pwittchen.swipe.library.Swipe;
+import com.github.pwittchen.swipe.library.SwipeEvent;
 import com.github.pwittchen.swipe.library.SwipeListener;
 import com.imaginecup.bug5.vista.R;
 import com.imaginecup.bug5.vista.fragment.MainFragment;
+import com.imaginecup.bug5.vista.fragment.emergency.SmartHomeFragment;
 import com.imaginecup.bug5.vista.utils.Constants;
 import com.mapzen.speakerbox.Speakerbox;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,14 +27,22 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvStatus;
 
+    int x = 0;
+    ArrayList<Fragment> screenList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.contentContainer, MainFragment.newInstance(), Constants.Fragments.MAIN FRAGMENT)
-                .commit();*/
+        //Add fragment to the List
+        screenList = new ArrayList<>();
+        screenList.add(MainFragment.newInstance());
+        screenList.add(SmartHomeFragment.newInstance());
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, MainFragment.newInstance())
+                .commit();
 
         tvStatus = (TextView) findViewById(R.id.tvStatus);
 
@@ -34,42 +50,49 @@ public class MainActivity extends AppCompatActivity {
         speakerbox = new Speakerbox(getApplication());
         swipe.addListener(new SwipeListener() {
             @Override public void onSwipingLeft(final MotionEvent event) {
-                tvStatus.setText("SWIPING LEFT");
+                //tvStatus.setText("SWIPING LEFT");
                 speakerbox.play("SWIPING LEFT");
             }
 
             @Override public void onSwipedLeft(final MotionEvent event) {
-                tvStatus.setText("SWIPED LEFT");
+                //tvStatus.setText("SWIPED LEFT");
                 speakerbox.play("SWIPED LEFT");
+                x--;
             }
 
             @Override public void onSwipingRight(final MotionEvent event) {
-                tvStatus.setText("SWIPING RIGHT");
+                //tvStatus.setText("SWIPING RIGHT");
                 speakerbox.play("SWIPING RIGHT");
+
+                Log.e("SwipeLog", "x : " + x);
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, SmartHomeFragment.newInstance()).commit();
+                x++;
+
             }
 
             @Override public void onSwipedRight(final MotionEvent event) {
-                tvStatus.setText("SWIPED RIGHT");
+                //tvStatus.setText("SWIPED RIGHT");
                 speakerbox.play("SWIPED RIGHT");
             }
 
             @Override public void onSwipingUp(final MotionEvent event) {
-                tvStatus.setText("SWIPING UP");
-                speakerbox.play("SWIPING UP");
+//                tvStatus.setText("SWIPING UP");
+                speakerbox.play("SWIPING up");
             }
 
             @Override public void onSwipedUp(final MotionEvent event) {
-                tvStatus.setText("SWIPED UP");
-                speakerbox.play("SWIPED UP");
+//                tvStatus.setText("SWIPED UP");
+               speakerbox.play("SWIPED up");
             }
 
             @Override public void onSwipingDown(final MotionEvent event) {
-                tvStatus.setText("SWIPING DOWN");
+//                tvStatus.setText("SWIPING DOWN");
                 speakerbox.play("SWIPING DOWN");
             }
 
             @Override public void onSwipedDown(final MotionEvent event) {
-                tvStatus.setText("SWIPED DOWN");
+//                tvStatus.setText("SWIPED DOWN");
                 speakerbox.play("SWIPED DOWN");
             }
         });
@@ -79,5 +102,9 @@ public class MainActivity extends AppCompatActivity {
     @Override public boolean dispatchTouchEvent(MotionEvent event) {
         swipe.dispatchTouchEvent(event);
         return super.dispatchTouchEvent(event);
+    }
+
+    private void switchScreenFragment(int position) {
+
     }
 }
